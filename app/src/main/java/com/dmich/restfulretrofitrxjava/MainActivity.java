068074e1a512a8cf6fb55ae36b7784e272278068
stemichael <dmichaelst@gmail.com>
2017-07-09 20:14:32 +0100

    unsubscribed observer

--- app/src/main/java/com/dmich/restfulretrofitrxjava/MainActivity.java
+++ app/src/main/java/com/dmich/restfulretrofitrxjava/MainActivity.java
@@ -10,12 +10,14 @@ import com.dmich.restfulretrofitrxjava.network.WeatherDataService;
 import com.dmich.restfulretrofitrxjava.network.retrofit.ApiClient;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
+    private Disposable subscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +32,21 @@ public class MainActivity extends AppCompatActivity {
 
         final WeatherDataService weatherDataService = new WeatherDataService(new ApiClient().getInstance());
 
-        weatherDataService.getWeatherData("London")
+        subscription = weatherDataService.getWeatherData("London")
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(weatherData -> {
                     mAdapter = new RecyclerViewAdapter(weatherData.getWeather());
                     mRecyclerView.setAdapter(mAdapter);
                 });
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (subscription != null && !subscription.isDisposed()) {
+            subscription.dispose();
+        }
+
+        super.onDestroy();
     }
 }
